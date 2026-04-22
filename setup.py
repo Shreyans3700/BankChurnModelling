b1a8a6d07506a8efa8a8b6f4718321d3fe78d74c
Shreyans3700 <pandeyshreyansh46@gmail.com>
2026-04-22 18:50:55 +0530

    Basic setup done for project logging and exception handling.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="BankChurnModelling",
+    version="0.0.1",
+    packages=["BankChurnModelling"],
+    author="Shreyansh Pandey",
+    description="A package for bank churn modelling using deep learning techniques.",
+    author_email="pandeyshreyansh46@gmail.com"
+)
